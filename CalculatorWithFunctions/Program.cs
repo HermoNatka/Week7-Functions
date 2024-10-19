fd7986fea3e9acd6febf563a886680778e397b23
Hermo Natka <hermo.natka@tktk.ee>
2024-10-19 19:48:55 +0300

    week 7

---
+++ CalculatorWithFunctions/Program.cs
@@ -0,0 +1,37 @@
+﻿int a = 5;
+int b = 6;
+
+Add(a, b);
+Add(10, 100);
+Add(555666, 556666);
+
+Multiply(a, b);
+Multiply(10, 100);
+Multiply(5555, 5566);
+
+Substract(a, b);
+Substract(10, 100);
+Substract(666, 555);
+
+Divide(a, b);
+Divide(10, 100);
+Divide(600, 100);
+static void Add(int x, int y)
+{
+    Console.WriteLine($"{x} + {y} = {x + y}");
+}
+
+static void Multiply(int x, int y)
+{
+    Console.WriteLine($"{x} * {y} = {x * y}");
+}
+
+static void Substract(int x, int y)
+{
+    Console.WriteLine($"{x} - {y} = {x - y}");
+}
+
+static void Divide(int x, int y)
+{
+    Console.WriteLine($"{x} / {y} = {x / y}");
+}
---
+++ Week7Functions/Program.cs
@@ -0,0 +1,14 @@
+﻿string hello = "Hello, world!";
+string panic = "Dont panic!";
+
+PrintStringFiveTimes(hello);
+PrintStringFiveTimes(panic);
+PrintStringFiveTimes("Batman");
+
+static void PrintStringFiveTimes(string someString)
+{
+    for (int i = 0; i < 5; i++)
+    {
+        Console.WriteLine(someString);
+    }
+}
